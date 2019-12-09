d8fde235bac5a8a0ce98e9d43eda19e545d137f4
John Stilley <1831479+theJollySin@users.noreply.github.com>
2019-12-09 12:38:21 -0500

    Cleaning up setup.py files

--- setup.py
+++ setup.py
@@ -16,8 +16,6 @@ setup(
     url = "https://github.com/theJollySin/morgue_librarian",
     long_description=readme,
     long_description_content_type='text/markdown',
-    #py_modules=["morgue_librarian"],
-    #entry_points={'console_scripts': ['morgue_librarian=morgue_librarian:main']},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
@@ -27,7 +25,6 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Natural Language :: English"
     ],
