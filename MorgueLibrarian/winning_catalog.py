1fd5f2952ee534041c9f971dd5fa62d914af52bc
theJollySin <john_science@protonmail.com>
2019-12-09 21:02:22 -0500

    Placeholder for catalog searcher

---
+++ MorgueLibrarian/winning_catalog.py
@@ -0,0 +1,15 @@
+from sys import argv
+
+
+def main():
+    """
+    1. parse input commandline for winning build/run info
+    2. open/read all winning runs in /data/, bzip or not
+    3. print lines that match search criteria
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
