162417701661e8e0b15eb5c4c41a17e6964e64c2
theJollySin <john_science@protonmail.com>
2019-12-09 21:19:56 -0500

    Outlining logic

--- MorgueLibrarian/winning_catalog.py
+++ MorgueLibrarian/winning_catalog.py
@@ -6,6 +6,11 @@ def main():
     1. parse input commandline for winning build/run info
     2. open/read all winning runs in /data/, bzip or not
     3. print lines that match search criteria
+      a) exact matches
+      b) wildcard match any
+      c) match any in a list
+      d) range operators from Python syntax: < > <= >= !=
+        i) perhaps we can use eval() here, to build lambdas?
     """
     pass
 
