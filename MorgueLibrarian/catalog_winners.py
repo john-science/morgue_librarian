2c749c78a85b2bcf6e854dce2625d30e5ac1f468
theJollySin <john_science@protonmail.com>
2020-01-25 16:37:17 -0500

    Flushing out the design goals

---
+++ MorgueLibrarian/catalog_winners.py
@@ -0,0 +1,28 @@
+""" Catalog all the Winning Morgues you have collected
+
+The Idea:
+
+First this script prints out some basics about all the winning morgues it can find:
+
+ * How many winners? What's the oldest? How many of each number of runes?
+
+Then this script will print out some stats on the winning morgues sorted based on user input:
+
+* species
+* background
+* god
+* number of runes
+* game version
+
+Ideally, we would make this very flexible so users can design their own queries, within the interests of looking through winning morgue to learn how to play.
+ 
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
